01261d7d3a65c9425db13ba8a1d4322004341fea
Olivier Basquin <olivier.basquin.iphone@gmail.com>
2021-06-28 22:15:37 +0200

    maj controllers

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -43,37 +43,6 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 
-// Création du like et dislike
-exports.likeSauce = (req, res, next) => {    
-    const like = req.body.like;
-    if(like === 1) { // Option like
-        Sauce.updateOne({_id: req.params.id}, { $inc: { likes: 1}, $push: { usersLiked: req.body.userId}, _id: req.params.id })
-        .then( () => res.status(200).json({ message: 'Vous aimez cette sauce !' }))
-        
-        .catch( error => res.status(400).json({ error}))
-    } else if(like === -1) { // Option dislike
-        Sauce.updateOne({_id: req.params.id}, { $inc: { dislikes: 1}, $push: { usersDisliked: req.body.userId}, _id: req.params.id })
-        .then( () => res.status(200).json({ message: 'Vous n\'aimez pas cette sauce !' }))
-        .catch( error => res.status(400).json({ error}))
-
-    } else {    // Annuler like ou dislike
-        Sauce.findOne( {_id: req.params.id})
-        .then( sauce => {
-            if( sauce.usersLiked.indexOf(req.body.userId)!== -1){
-                 Sauce.updateOne({_id: req.params.id}, { $inc: { likes: -1},$pull: { usersLiked: req.body.userId}, _id: req.params.id })
-                .then( () => res.status(200).json({ message: 'Vous n\'aimez plus cette sauce !' }))
-                .catch( error => res.status(400).json({ error}))
-                }
-            else if( sauce.usersDisliked.indexOf(req.body.userId)!== -1) {
-                Sauce.updateOne( {_id: req.params.id}, { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId}, _id: req.params.id})
-                .then( () => res.status(200).json({ message: 'Vous aimez cette sauce !' }))
-                .catch( error => res.status(400).json({ error}))
-                }           
-        })
-        .catch( error => res.status(400).json({ error}))             
-    }   
-};
-
 // Récupération de toutes les sauces
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
@@ -87,3 +56,7 @@ exports.getOneSauce = (req, res, next) => {
     .then( sauce => res.status(200).json(sauce))
     .catch( error => res.status(404).json({ error }))
 };
+
+
+// Création du like et dislike
+
